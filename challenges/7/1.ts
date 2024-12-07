31700ba872a0d2ed38101de604301529f0813dfe
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-12-07 19:20:36 +0100

    Day 7

---
+++ challenges/7/1.ts
@@ -0,0 +1,44 @@
+const input = await Deno.readTextFile("input/7.txt");
+
+const OPERATORS: ((n1: number, n2: number) => number)[] = [
+  (n1, n2) => n1 + n2,
+  (n1, n2) => n1 * n2,
+];
+
+const operations = input.split("\n").map((operation) => {
+  const [resultString, operandsString] = operation.split(": ");
+  const result = parseInt(resultString);
+  const operands = operandsString.split(" ").map((s) => parseInt(s));
+  return { result, operands };
+});
+
+const validOperation = (operation: { result: number; operands: number[] }) => {
+  const intermediateResults = [];
+  let i = -1;
+  for (const operand of operation.operands) {
+    i++;
+    if (i === 0) {
+      intermediateResults.push(operand);
+      continue;
+    }
+    const isLast = i === operation.operands.length - 1;
+    for (const result of [...intermediateResults]) {
+      for (const operator of OPERATORS) {
+        if (!isLast) {
+          intermediateResults.push(operator(result, operand));
+        } else if (operator(result, operand) === operation.result) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+};
+
+let sum = 0;
+operations.forEach((operation) => {
+  if (validOperation(operation)) {
+    sum += operation.result;
+  }
+});
+console.log(sum);
