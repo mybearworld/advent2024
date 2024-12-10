51cbe617dc64b45ba9be59984de4df522063b64b
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-12-10 07:44:01 +0100

    Day 10

---
+++ challenges/10/1.ts
@@ -0,0 +1,48 @@
+const input = await Deno.readTextFile("input/10.txt");
+const grid = input.split("\n");
+
+const BASE_HEIGHT = "0";
+const HEIGHTS = ["1", "2", "3", "4", "5", "6", "7", "8", "9"] as const;
+const DIRECTIONS: (([x, y]: [number, number]) => [number, number])[] = [
+  ([x, y]) => [x, y - 1],
+  ([x, y]) => [x + 1, y],
+  ([x, y]) => [x, y + 1],
+  ([x, y]) => [x - 1, y],
+];
+
+let totalScore = 0;
+let y = -1;
+for (const row of grid) {
+  y++;
+  let x = -1;
+  for (const cell of row) {
+    x++;
+    if (cell !== BASE_HEIGHT) {
+      continue;
+    }
+    let forks: [number, number][] = [[x, y]];
+    for (const height of HEIGHTS) {
+      const nextForks: Set<`${number},${number}`> = new Set();
+      for (const fork of forks) {
+        for (const direction of DIRECTIONS) {
+          const [nextX, nextY] = direction(fork);
+          if (grid[nextY]?.[nextX] === height) {
+            nextForks.add(`${nextX},${nextY}`);
+          }
+        }
+      }
+      forks = nextForks
+        .values()
+        .map(
+          (value) =>
+            value.split(",").map((p) => parseInt(p)) as [number, number]
+        )
+        .toArray();
+      if (nextForks.size === 0) {
+        break;
+      }
+    }
+    totalScore += forks.length;
+  }
+}
+console.log(totalScore);
